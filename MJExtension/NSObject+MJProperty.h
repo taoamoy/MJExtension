51ea049b29b001a754325d05b34758566448630d
taohong <taohong@qq.com>
2016-10-06 11:31:05 +0800

    修复一些小问题

--- MJExtension/NSObject+MJProperty.h
+++ MJExtension/NSObject+MJProperty.h
@@ -46,12 +46,7 @@ typedef id (^MJNewValueFromOldValue)(id object, id oldValue, MJProperty *propert
 + (void)mj_setupNewValueFromOldValue:(MJNewValueFromOldValue)newValueFormOldValue;
 + (id)mj_getNewValueFromObject:(__unsafe_unretained id)object oldValue:(__unsafe_unretained id)oldValue property:(__unsafe_unretained MJProperty *)property;
 
-#pragma mark - key配置
-/**
- *  将属性名换为其他key去字典中取值
- *
- *  @param replacedKeyFromPropertyName 将属性名换为其他key去字典中取值
- */
+
 + (void)mj_setupReplacedKeyFromPropertyName:(MJReplacedKeyFromPropertyName)replacedKeyFromPropertyName;
 /**
  *  将属性名换为其他key去字典中取值
